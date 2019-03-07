ff3d1d2dcaa707545136fbbeb93dff3934ee23fa
Ivan Aldwin A. Cristobal <ivanaldwin996@gmail.com>
2019-03-07 16:25:26 +0800

    updated login js

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,6 @@
 (function(){
   $('#login').on('click', function (e) {
+    $(".spinner-wrapper").show();
     e.preventDefault();
     if( $('#loginEmail').val() != '' && $('#loginPassword').val() != '' ){
       //login the user
@@ -11,12 +12,25 @@
       firebase.auth().signInWithEmailAndPassword(data.email, data.password)
         .then(function(authData) {
           auth = authData;
-          alert('Login Successful! Welcome'); //ToDo Trigger progress bar here
+          document.getElementById("error-desc").innerHTML = "";
+          $(".spinner-wrapper").hide();
           window.location.replace('redirect.html');
         })
         .catch(function(error) {
-          console.log("Login Failed!", error);
-          alert("Login Failed");
+          var errorCode = error.code;
+          if(errorCode == 'auth/invalid-email'){
+            document.getElementById("error-desc").innerHTML = "Invalid Email Format! Try Again.";
+            $(".spinner-wrapper").hide();
+          }
+          else if(errorCode == 'auth/user-not-found'){
+            document.getElementById("error-desc").innerHTML = "That email is not associated with any admin account. Try Again.";
+            $(".spinner-wrapper").hide();
+          }
+          else if(errorCode == 'auth/wrong-password'){
+            document.getElementById("error-desc").innerHTML = "Wrong Password! Try Again.";
+            $(".spinner-wrapper").hide();
+          }
+          console.log("Login Failed!" + errorCode);
         });
     }
   });
