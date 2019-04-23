af7299dbaa6989105d08fc381a75b7bce4128ea9
Ivan Aldwin A. Cristobal <ivanaldwin996@gmail.com>
2019-04-23 23:12:50 +0800

    pre defense-2 changes

--- public/js/notifsDist.js
+++ public/js/notifsDist.js
@@ -1,6 +1,6 @@
 function reports() {
     var reportsRef = firebase.database().ref("reports");
-    reportsRef.on('child_added', function (snapshot) {
+    reportsRef.on('child_changed', function (snapshot) {
         document.getElementById('rep-counter').innerHTML = "!";
         console.log(snapshot.val());
     })
